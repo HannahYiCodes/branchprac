84134141ee573bcce04bb99d06db6f60219c773e
Hannah Yi <hannahyi@Hannahs-MBP.fios-router.home>
2022-06-09 18:22:08 -0400

    first commit

---
+++ branchprac.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+
+const mysql = require('mysql')
+
+const db = mysql.createConnection({
+    user: 'root',
+    host: 'localhost',
+    password: 'password',
+    database: 'countries',
+})
+
+app.get('/select', (req, res) => {
+    db.query(
+        "SELECT * FROM countries", (err, result) => {
+        if (err) {
+            console.log(err)
+        }
+        res.send(result)
+    })
+})
+
+app.post('/insert', (req, res) => {
+    const countryName = "Bulgaria";
+    const population = 69000000;
+
+
+    db.query('INSERT INTO countries (countryName, population) VALUES (?, ?)', [countryName, population], (err, result) => {
+        if (err) {
+            console.log(err)
+        }
+        res.send(result)
+    })
+})
+
+app.listen(3001, () => {
+    console.log('server running')
+})
